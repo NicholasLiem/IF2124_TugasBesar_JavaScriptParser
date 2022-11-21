7a0037560d2bfc0f5298638e3b8c6636ab127809
nathania01 <nathania.calista01@gmail.com>
2022-11-21 14:23:25 +0700

    Add : CFG rules for Switch Case

--- tes.js
+++ tes.js
@@ -1,8 +1,15 @@
-function func4 (a,b){
-    return a;
+var x = 3
+function tes(a,b){
+    return a
 }
-var x = 0
-switch(true){
-    case(1):
-        console.log("3")
+switch(x){
+    case tes(3,4): 
+        console.log("hai")
+        break
+    default : 
+        console.log("default")
+    break
 }
+
+
+
