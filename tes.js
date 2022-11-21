6062343cb334e1cc5993e2af145f550e8b4d2d65
nathania01 <nathania.calista01@gmail.com>
2022-11-21 09:08:45 +0700

    add : CFG rules for if and switch

--- tes.js
+++ tes.js
@@ -1,39 +1,8 @@
-    console.log("hai = 1")
-
-haha = ( a , b = 9) => {
-    let c = a+b+5;
-    return (c += 5)
+function func4 (a,b){
+    return a;
 }
-
-console.log(haha(5))
-
-let a = true
-let b = (a) + true
-let c = [1,2,3,4,5]
-
-console.log(!"aaaa")
-
-let i = 0
-
-
-
-let y = 0
-y += c
-
-do{ console.log("aaaaaaaa") }while (x = 5)
-
-// for ((x *= 7); i < 40; i ++){
-//     console.log(i)
-// }
-
-console.log(y)
-class Car{
-    constructor(height,width=2){
-        this.area 
-        console.log(this.area)
-    }
+var x = 0
+switch(true){
+    case(1):
+        console.log("3")
 }
-
-var car = new Car(4)
-
-var Hai = "tes"
