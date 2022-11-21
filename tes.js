dffdaaecea0656efba782918bdfb14f79e0c2ba2
nathania01 <nathania.calista01@gmail.com>
2022-11-21 14:45:14 +0700

    add : try catch finally rules

--- tes.js
+++ tes.js
@@ -1,15 +1,6 @@
-var x = 3
-function tes(a,b){
-    return a
-}
 switch(x){
-    case tes(3,4): 
-        console.log("hai")
-        break
-    default : 
-        console.log("default")
-    break
+    case 1 : 
+        function temp(x){
+            return x;
+        }
 }
-
-
-
