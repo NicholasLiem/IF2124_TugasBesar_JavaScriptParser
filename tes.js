9bea770f88a0312f41ff258ad9738acd8873898c
nathania01 <nathania.calista01@gmail.com>
2022-11-24 20:12:15 +0700

    fix : bugs in switch case rules

--- tes.js
+++ tes.js
@@ -1,9 +1,30 @@
 var x = 2
-while(x <= 2){
-    console.log(x)
-    x++
+
+function teriak(){
+    console.log("haiii")
 }
-// while(x <= 5){
-//     console.log(x)
-//     x++
-// }
+switch(
+    x
+    )
+    {
+    
+    case 1 : 
+        teriak()
+        break 
+    case 2 :
+        y = 1
+        switch(y){
+            case 1 : 
+                console.log("switch acse 2")
+                for(i = 0 ; i < 3;i++){
+                    console.log(i)
+                }
+            case 2 :
+                console.log("switch case 3")
+        }
+    case 3 : 
+        console.log("tiga")
+        break
+    case 4 : 
+        console.log("default")
+}
