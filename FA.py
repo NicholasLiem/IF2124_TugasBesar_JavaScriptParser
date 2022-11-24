553919f77fa7e6ee9677d3260e03b383bca8f055
energizedNachos <16521108@mahasiswa.itb.ac.id>
2022-11-24 16:36:18 +0700

    feat: update FA sementara

--- FA.py
+++ FA.py
@@ -9,48 +9,121 @@ def check_var(input):
     for char in canBeFirstChar:
         if (firstChar == char):
             flag = True
-    if (input == 'false' or input == 'true'):
-        flag = False
     return flag
 
-# EBNF Form
-# digits     = digit { digit }
-# number     = [ "+" | "-" ] ( digits [ "." [ digits ] ] | "." digits )
-# operator   = "+" | "-" | "*" | "/" | "<" | ">"
-# expression = number { operator number }
-
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-operator = ['+', '-', '*' , '/', '<', '>' , '%', '^', '&', '>>>', 
+operator = ['+', '-', '*' , '/', '<', '>', '%', '^', '&', '>>>', 
             '<<', '>>', '|', '~', '==', '>=', '<=', '===', '!=', '!==',
-            '&&', '||', '!', '??']
+            '&&', '||', '!', '??', '**']
+single_operator = ['+', '-', '*', '/', '<', '>', '%', '^', '&', '>',
+                    '<', '|', '~', '=', '!', '?']
 
-# Note: Perlu cek bilangan koma, kalo < bedain sama <<, sama >, >>, >>>
-# Expression: Number {Operator} Number
-
-def check_arithmetic_expression(input):
+def check_arithmetic_expression(input : str) -> bool:
     # Menghapus spasi
     input = input.replace(' ', '')
-
-def q0(input) -> bool:
-    if (len(input) == 0):
-        print("Woi")
-        return True
-    if(input[0] == '.'):
-        q1(input[1:])
-    if (input[0] in digits):
-        q0(input[1:])
-
-def q1(input):
-    firstChar = input[0]
-    if(firstChar == '.'):
+    if(input[0] in single_operator):
         return False
     else:
-        for char in digits:
-            if (firstChar == char):
-                q0(input[1:])
+        return q0(input)
 
-def q2(input):
-    ...
-    # Cek expression
+def q0(input : str) -> bool:
+    if (len(input) == 0):
+        return True
+    else:
+        if (input[0] in digits):
+            return q0(input[1:])
+        elif (input[0] == '.'):
+            return q1(input[1:])
+        elif (input[0] in single_operator):
+            return q2(input)
+        else:
+            return False
+        
+def q1(input : str) -> bool:
+    if (len(input) == 0):
+        return True
+    else:
+        if(input[0] in digits):
+            return q1(input[1:])
+        elif(input[0] in single_operator):
+            return q2(input)
+        else:
+            return False
 
-print(q0('12'))
+# Cek expression
+def q2(input : str) -> bool:
+    if(len(input) == 0):
+        return False
+    else:
+        firstChar = input[0]
+        if (firstChar == '<'):
+            if(len(input) > 2):
+                if(input[1] == '<' or input[1] == '='):
+                    return q0(input[2:])
+            if (len(input) > 1):
+                return q0(input[1:])
+
+        elif (firstChar == '>'):
+            if(len(input) > 3):
+                if(input[1] == '>' and input[2] == '>'):
+                    return q0(input[3:])
+            if (len(input) > 2):
+                if(input[1] == '>' or input[1] == '='):
+                    return q0(input[2:])
+            if (len(input) > 1):
+                return q0(input[1:])
+
+        elif (firstChar == '|'):
+            if(len(input) > 2):
+                if(input[1] == '|'):
+                    return q0(input[2:])
+            if(len(input) > 1):
+                return q0(input[1:])
+
+        elif (firstChar == '?'):
+            if(len(input) > 2):
+                if(input[1] == '?'):
+                    return q0(input[2:])
+            if(len(input) > 1):
+                return q0(input[1:])
+
+        elif (firstChar == '!'):
+            return q2(input[1:])
+
+        elif (firstChar == '~'):
+            return q2(input[1:])
+
+        elif (firstChar == '*'):
+            if(len(input) > 2):
+                if(input[1] == '*'):
+                    return q0(input[2:])
+            if(len(input) > 1):
+                return q0(input[1:])
+
+        elif (firstChar == '='):
+            if(len(input) > 3):
+                if(input[1] == '=' and input[2] == '='):
+                    return q0(input[3:])
+            if (len(input) > 2):
+                if(input[1] == '='):
+                    return q0(input[2:])
+            if (len(input) > 1):
+                return q0(input[1:])
+
+        elif (firstChar == '/'):
+            ...
+            
+        elif (firstChar == '+' or firstChar == '-' or firstChar == '^' or firstChar == '%'):
+            return q0(input[1:])
+
+        elif (firstChar in digits or firstChar == '.'):
+            if(firstChar in digits):
+                return q0(input[1:])
+            else:
+                return q1(input[1:])
+        else:
+            return False
+
+# Kasus OPERATOR baru ~ ato ! harus expression dulu, OPERATOR sendiri tanpa angka
+
+print(check_arithmetic_expression('1 + 1'))
